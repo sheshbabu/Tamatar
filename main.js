976efc4b0ef5dd07a5fb27094c20b742bfb6b9d1
Shesh <sheshbabu@gmail.com>
2015-04-06 20:57:54 +0530

    Added notification instead of window.focus

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ var onTimerEnd = function() {
     startStopEl.textContent = 'Start'
     isRunning = false;
     enablePaneSwitching();
-    chrome.app.window.current().focus();
+    chrome.notifications.create('', {
+        iconUrl: 'tamatar-notification.png',
+        title: 'Tamatar',
+        message: 'Done',
+        type: 'basic'
+    }, function() {});
 }
 
 var timer;
