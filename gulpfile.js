104ad7393efdec9d97b04ddc7ffb629db084467a
Shesh <sheshbabu@gmail.com>
2015-04-16 23:04:44 +0530

    Refactored gulpfile and added gulp-concat plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 
 // Plugins
 var jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
     jasmine = require('gulp-jasmine-phantom');
 
 // Lint
@@ -11,13 +12,30 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('integrationTests', function() {
-    return gulp.src('tests/specs/*.js')
+gulp.task('compile-unit-test', function() {
+    return gulp.src(['src/scripts/timer.js', 'tests/specs/timer.spec.js'])
+        .pipe(concat('unit-test-specs.js'))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('unit-test', ['compile-unit-test'], function() {
+    return gulp.src('unit-test-specs.js')
+        .pipe(jasmine());
+});
+
+gulp.task('compile-integration-test', function() {
+    return gulp.src(['src/scripts/timer.js', 'src/scripts/pomodoro.js', 'tests/specs/pomodoro.spec.js'])
+        .pipe(concat('integration-test-specs.js'))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('integration-test', ['compile-integration-test'], function() {
+    return gulp.src('integration-test-specs.js')
         .pipe(jasmine({
             integration: true,
-            vendor: ['tests/lib/jquery-2.1.3.min.js', 'tests/lib/jasmine-jquery.js', 'tests/specs/pomodoro-spec.js', 'src/scripts/timer.js', 'src/scripts/pomodoro.js']
+            vendor: ['tests/lib/jquery-2.1.3.min.js', 'tests/lib/jasmine-jquery.js']
         }));
 });
 
 // Default
-gulp.task('default', ['lint', 'integrationTests']);
+gulp.task('default', ['lint', 'unit-test', 'integration-test']);
